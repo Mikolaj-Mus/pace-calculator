26c32722fc13d06257f4074c4891db48e782a56a
Mikolaj-Mus <mus_mikolaj@wp.pl>
2023-11-29 11:41:52 +0100

    Add result component

--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -1,21 +1,33 @@
-import { calculateTime } from "../../data";
+import { calculatePace, calculateTime } from "../../data";
+import "./Result.css";
 
 export default function Result({ input }) {
-  const pace = calculateTime(input);
+  const pace = calculatePace(input);
+  const distances = [1, 1.6, 3, 5, 10, 21.1, 42.2];
 
   return (
-    // <table id="result">
-    //   <thead>
-    //     <tr>
-    //       <th>Distance</th>
-    //       <th>Time</th>
-    //       <th>Pace</th>
-    //     </tr>
-    //   </thead>
-    //   <tbody></tbody>
-    // </table>
     <>
-      <p>{pace}</p>
+      <table id="result">
+        <thead>
+          <tr>
+            <th>Distance</th>
+            <th>Time</th>
+            <th>Pace</th>
+          </tr>
+        </thead>
+        <tbody>
+          {distances.map((distance) => {
+            const time = calculateTime({ distance, pace });
+            return (
+              <tr key={distance}>
+                <td>{distance}</td>
+                <td>{time}</td>
+                <td>{pace}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </>
   );
 }
--- src/data.js
+++ src/data.js
@@ -1,15 +1,59 @@
-export function calculateTime({ distance, time }) {
-  const [hours, minutes, seconds] = time.split(":");
+export function calculateTime({ distance, pace }) {
+  const paceComponents = pace.split(":");
 
-  let totalMinutes = +hours * 60 + +minutes + +seconds / 60;
-
-  let paceMinutes = Math.floor(totalMinutes / +distance);
-  let paceSeconds = Math.floor((totalMinutes % distance) * 6);
-
-  if (paceSeconds < 10) {
-    paceSeconds = `0${paceSeconds}`;
+  if (
+    paceComponents.length !== 2 ||
+    isNaN(+paceComponents[0]) ||
+    isNaN(+paceComponents[1])
+  ) {
+    // Handle invalid pace input
+    return "--:--:--";
   }
-  let pace = `${paceMinutes}:${paceSeconds}`;
 
-  return pace;
+  const [paceMinutes, paceSeconds] = paceComponents;
+
+  let totalSeconds = +paceMinutes * 60 + +paceSeconds;
+  let timeInSeconds = totalSeconds * +distance;
+  let hours = Math.floor(timeInSeconds / 3600);
+  let minutes = Math.floor((timeInSeconds % 3600) / 60);
+  let seconds = Math.floor(timeInSeconds % 60);
+
+  return `${hours.toString().padStart(2, "0")}:${minutes
+    .toString()
+    .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+}
+
+export function calculatePace({ distance, time }) {
+  const timeComponents = time.split(":");
+
+  if (
+    timeComponents.length !== 3 ||
+    isNaN(+timeComponents[0]) ||
+    isNaN(+timeComponents[1]) ||
+    isNaN(+timeComponents[2])
+  ) {
+    // Handle invalid time input
+    return "--:--";
+  }
+
+  if (+distance === 0 || +distance < 0) {
+    return "--:--";
+  }
+
+  const [hours, minutes, seconds] = timeComponents;
+
+  let totalSeconds = +hours * 3600 + +minutes * 60 + +seconds;
+  let paceInSeconds = totalSeconds / +distance;
+
+  let paceMinutes = Math.floor(paceInSeconds / 60);
+  let paceSeconds = Math.round(paceInSeconds % 60);
+
+  if (paceSeconds === 60) {
+    paceSeconds = 0;
+    paceMinutes++;
+  }
+
+  return `${paceMinutes.toString().padStart(2, "0")}:${paceSeconds
+    .toString()
+    .padStart(2, "0")}`;
 }
